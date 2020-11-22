a586ab13780cfb7c56bb68545ccacd39162c25e9
Andrew Varnavsky <39239738+Andrey0189@users.noreply.github.com>
2020-11-22 23:08:03 +0300

    Add files via upload

--- Commands/avatar.js
+++ Commands/avatar.js
@@ -6,7 +6,7 @@ module.exports = {
   example: 'avatar @чел#1234',
   run: (message, args) => {
     const matchArgs = new RegExp(args[0], 'i')
-    const member = message.mentions.members.first() || message.guild.members.cache.find(m => m.user.tag.match(matchArgs)) || message.member;
+    const member = message.mentions.members.first() || message.guild.members.cache.find(m => args[0] && m.user.tag.match(matchArgs)) || message.member;
     message.channel.send(member.user.avatarURL({size: 1024, dynamic: true}));
   }
 };
